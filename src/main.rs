8b7840c8e0b7d669b34e061a6aa3075e864b350a
Ash Entwisle <ash.entwisle@protonmail.com>
2024-06-29 20:35:27 +0100

    added piping
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::{self, BufRead}};
 
 use clap::{arg, Command};
 
 fn cmd() -> Command {
     Command::new("angr")
         .about("A command line tool to to count the frequency of bigrams in a text file")
-        .arg(arg!(file: <FILE> "The input file to read from"))
-        .arg(arg!(n: -n --ngram <N> "The size of the n-gram to count").default_value("2"))
+        .arg(arg!(file: -f --file <FILE> "The input file to read from"))
+        .arg(arg!(n: -n --ngram <N> "The size of the n-gram to count"))
         .arg(arg!(output: -o --output <OUTPUT> "The output file to write to"))
         .arg(arg!(silent: -s --silent "Silence all output"))
 }
@@ -16,12 +16,22 @@ fn main() {
     let matches = cmd().get_matches();
     let blank: String = "".to_string();
 
-    let file: &String   = matches.get_one::<String>("file").unwrap();
+    // TODO: fix piping input
+    let stdin = io::stdin();
+    let mut pipe: String = "".to_string();
+    
+    for line in stdin.lock().lines() {
+        let line = line.expect("Could not read line from standard in");
+        pipe.push_str(&line);
+    }
+
+    let file: &String   = matches.get_one::<String>("file").unwrap_or(&blank);
     let n: &u128        = matches.get_one::<u128>("n").unwrap_or(&2);
     let output: &String = matches.get_one::<String>("output").unwrap_or(&blank);
     let silent: &bool   = matches.get_one::<bool>("silent").unwrap_or(&false);
 
-    let text = std::fs::read_to_string(file).unwrap();
+    
+    let text = std::fs::read_to_string(file).unwrap_or(pipe);
 
     // for each word, for each character, for each ngram, count it
     let mut counts: HashMap<String, usize> = std::collections::HashMap::new();
