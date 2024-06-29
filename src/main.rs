eed1692992aa844f0ef2cad8ffe340112c422481
Ash Entwisle <ash.entwisle@protonmail.com>
2024-06-29 20:01:52 +0100

    added temp code
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+use std::{collections::HashMap, hash::Hash, str::FromStr};
+
+use clap::{arg, builder::PossibleValue, value_parser, Command, ValueEnum};
+use serde_json::value;
+
+fn cmd() -> Command {
+    Command::new("bga")
+        .about("A command line tool to to count the frequency of bigrams in a text file")
+        .arg(arg!(file: <FILE> "The input file to read from"))
+        .arg(arg!(n: -n --ngram <N> "The size of the n-gram to count").default_value("2"))
+        .arg(arg!(output: -o --output <OUTPUT> "The output file to write to"))
+        .arg(arg!(silent: -s --silent "Silence all output"))
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let matches = cmd().get_matches();
+
+    let blank: String = "".to_string();
+
+    let file = matches.get_one::<String>("file").unwrap();
+    let n = u32::from_str(matches.get_one::<String>("n").unwrap().as_str()).unwrap();
+    let output = matches.get_one::<String>("output").unwrap_or(&blank);
+    let silent = matches.get_one::<bool>("silent").unwrap_or(&false);
+
+    let text = std::fs::read_to_string(file).unwrap();
+
+    // for each word, for each character, for each ngram, count it
+    let mut counts: HashMap<String, usize> = std::collections::HashMap::new();
+
+    for word in text.split_whitespace() {
+        let word = word.chars().collect::<Vec<char>>();
+        for i in 0..word.len() {
+            let mut ngram = String::new();
+            for j in 0..n {
+                if (i + j as usize) < word.len() {
+                    ngram.push(word[i + j as usize]);
+                }
+            }
+            let count = counts.entry(ngram).or_insert(0);
+            *count += 1;
+        }
+    }
+
+
+    // sort the counts
+    let mut counts = counts
+        .into_iter()
+        .filter(|(ngram, _)| ngram.len() == n as usize)
+        .collect::<Vec<(String, usize)>>();
+
+    counts.sort_by(|a, b| b.1.cmp(&a.1));
+
+
+    // print the counts
+    if !silent {
+        for (ngram, count) in &counts {
+            println!("{}: {}", ngram, count);
+        }
+    }
+
+    // write the counts to a file
+    if output != &blank {
+        let json = serde_json::to_string(&counts).unwrap();
+        std::fs::write(output, json).unwrap();
+    }
 }
